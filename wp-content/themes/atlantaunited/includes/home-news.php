1020ce1e9adc9ebb204514a5fcfb46cf245f0c3f
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2019-03-18 01:15:28 -0400

    added news section

--- wp-content/themes/atlantaunited/includes/home-news.php
+++ wp-content/themes/atlantaunited/includes/home-news.php
@@ -6,30 +6,23 @@
             $args = array(
                 "post_type" => "news",
                 // "category_name" => get_field('games_display_category'),
-                "posts_per_page" => 9,
+                "posts_per_page" => -1,
             );
             $query = new WP_Query($args);
 
             if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); 
             
+        ?>
 
-                $exploded = explode(' ', get_field('date_and_time'));
-                // print_r($exploded);
-                $date = $exploded[0] . ' ' . $exploded[1];
-                $time = $exploded[3] . ' ' . $exploded[4];
-            ?>
 
-            <li class="article">
-                <div class="date-wrapper">
-                    <p class="date"><?php echo $date ?></p>
+            <li class="article <?php the_field('classname') ?>">
+                <div class="img-wrapper">
+                    <img src="<?php the_field('image') ?>">
                 </div>
-                <span class="time"><?php echo $time ?></span>
-                <span class="otherteam"><?php echo get_field('other_team')['label'] ?></span>
-                <div class="img-container">
-                    <img src="<?php echo get_template_directory_uri()?>/dist/images/logos/<?php echo get_field('other_team')['value'] ?>.gif')">
+                <div class="headline">
+                    <span><?php the_field('headline') ?></span>
                 </div>
-                <span class="location"><?php echo get_field('location') ?></span>
-                <button class="game-btn">Tickets</button>
+
             </li>
 
         <?php endwhile; wp_reset_postdata(); endif; ?>
