28e0dcf4cdd6c7945a97afe17acf9d0ba022ebab
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2019-03-18 13:33:08 -0400

    finished css for mobile views

--- wp-content/themes/atlantaunited/includes/home-ranking.php
+++ wp-content/themes/atlantaunited/includes/home-ranking.php
@@ -6,22 +6,22 @@
     <div class="score-wrapper mid-section ranking">
 
         <div class="total">
-            <p>gp</p>
+            <p>G<span class="tablet-hide">ames</span></p>
             <span><?php echo get_field('games_played') ?></span>
         </div>
 
         <div class="wins">
-            <p>w</p>
+            <p>W<span class="tablet-hide">in</span></p>
             <span><?php echo get_field('wins') ?></span>
         </div>
         
         <div class="loss">
-            <p>l</p>
+            <p>L<span class="tablet-hide">oss</span></p>
             <span><?php echo get_field('loss') ?></span>
         </div>
         
         <div class="draws">
-            <p>d</p>
+            <p>D<span class="tablet-hide">raw</span></p>
             <span><?php echo get_field('draws') ?></span>
         </div>
     </div>
